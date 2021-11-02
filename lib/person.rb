ca24270e289105c07d082ac6f11fd133d502d379
Sam Bencivengo <sambencivengo@gmail.com>
2021-11-01 22:29:54 -0400

    commit tree ac49a57b30926913d60dd6c36b50abf8496637a9

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
 
+  def name=(name)
+    @name = name
+    #setting the arg to an instance variable
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+    
+
 end
